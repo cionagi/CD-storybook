2f9a2f8b577db35900122eaf26b20a1a4ca553b9
Luis Ignacio Zavala Castelblanco <cionagi@hotmail.cl>
2020-04-17 16:09:00 -0400

    Update readme (#19)
    Co-authored-by: Luis Zavala <>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Branch 2</h1>
         <h1>Branch 1</h1>
         <h1>Branch 3</h1>
         <img src={logo} className="App-logo" alt="logo" />
